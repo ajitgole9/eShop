6172eefae1b3cc58ebc97024ae4fb187287dee12
Ajit Gole <ajit.gole9@gmail.com>
2024-05-01 14:15:47 +0530

    Create Produt Changes

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, string Description, List<string> Categories, string Image, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Description = command.Description,
+            Categories = command.Categories,
+            Image = command.Image,
+            Price = command.Price,
+        };
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
+    }
+}
