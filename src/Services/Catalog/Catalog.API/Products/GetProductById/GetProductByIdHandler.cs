f7429a1ff3f9fb6b56afe90a27c61ce728c27c3c
Ajit Gole <ajit.gole9@gmail.com>
2024-05-05 09:20:07 +0530

    Added Get Prouct Endpoints

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product product);
+public class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQuery> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+
+        logger.LogInformation("GetProductByIdQueryHandler.Handle Called with Query {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.id, cancellationToken);
+
+        if(product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
